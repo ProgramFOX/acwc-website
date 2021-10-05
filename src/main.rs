f388c8074712944749d7bd18cea6af2c586e4b3a
Thomas Daniels <daniels.thomas@pm.me>
2021-10-05 22:19:51 +0200

    Extend registration time for ACWC 2021

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn context<'a>(maybe_session: &'a Option<Session>) -> HashMap<&'static str, &'a 
 
 fn registration_state() -> i32 {
     let register_start = Utc.ymd(2021, 9, 26).and_hms(0, 0, 0);
-    let register_end = Utc.ymd(2021, 10, 6).and_hms(0, 1, 0);
+    let register_end = Utc.ymd(2021, 10, 8).and_hms(23, 59, 59);
     let now = Utc::now();
     if now >= register_end {
         2 // registration ended
